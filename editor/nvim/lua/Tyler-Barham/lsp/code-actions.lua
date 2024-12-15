28888d1326098480ac3c6952e229900d31d49e81
Tyler Barham <tyler.r.barham@gmail.com>
2024-12-16 12:32:17 +1300

    Update whitespace to align args

--- editor/nvim/lua/Tyler-Barham/lsp/code-actions.lua
+++ editor/nvim/lua/Tyler-Barham/lsp/code-actions.lua
@@ -14,14 +14,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     local switchSourceHeader = 'call CurtineIncSw()'
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,              opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition,               opts)
-    vim.keymap.set('n', ',d',   vim.lsp.buf.type_definition,          opts)
-    vim.keymap.set('n', 'gt', vim.lsp.buf.hover,                    opts)
-    vim.keymap.set('n', 'gT', vim.lsp.buf.signature_help,           opts)
-    vim.keymap.set('n', 'gi', builtin.lsp_implementations,          opts)
-    vim.keymap.set('n', 'gr', builtin.lsp_references,               opts)
-    vim.keymap.set('n', 'gh', '<CMD>'..switchSourceHeader..'<CR>',  opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,                opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition,                 opts)
+    vim.keymap.set('n', ',d', vim.lsp.buf.type_definition,            opts)
+    vim.keymap.set('n', 'gt', vim.lsp.buf.hover,                      opts)
+    vim.keymap.set('n', 'gT', vim.lsp.buf.signature_help,             opts)
+    vim.keymap.set('n', 'gi', builtin.lsp_implementations,            opts)
+    vim.keymap.set('n', 'gr', builtin.lsp_references,                 opts)
+    vim.keymap.set('n', 'gh', '<CMD>'..switchSourceHeader..'<CR>',    opts)
 
     vim.keymap.set('n', ',wa',  vim.lsp.buf.add_workspace_folder,     opts)
     vim.keymap.set('n', ',wr',  vim.lsp.buf.remove_workspace_folder,  opts)
@@ -29,8 +29,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
 
-    vim.keymap.set('n', '<leader>rn',  vim.lsp.buf.rename,                   opts)
-    vim.keymap.set('n', '<leader>ca',  vim.lsp.buf.code_action,              opts)
+    vim.keymap.set('n', '<leader>rn',  vim.lsp.buf.rename,            opts)
+    vim.keymap.set('n', '<leader>ca',  vim.lsp.buf.code_action,       opts)
     -- vim.keymap.set('n', ',f',   function()
     --   vim.lsp.buf.format { async = true }
     -- end, opts)

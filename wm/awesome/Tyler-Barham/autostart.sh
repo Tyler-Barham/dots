ce63eee40308fb656ab9d2a0db1c63847a6ffe00
Tyler Barham <tyler.r.barham@gmail.com>
2024-12-20 12:50:15 +1300

    Add picom and nitrogen to autostart

--- wm/awesome/Tyler-Barham/autostart.sh
+++ wm/awesome/Tyler-Barham/autostart.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+
+function runonce {
+  if ! pgrep $1 ; then
+    $@&
+  fi
+}
+
 if xrandr | grep -q 'DP-1-2 connected' ; then
     xrandr --output DP-1-2 --primary
     xrandr --output DP-1-1 --right-of DP-1-2 --rotate right
     xrandr --output eDP-1 --off
 fi
+
+nitrogen --restore
+
+runonce "picom"
 

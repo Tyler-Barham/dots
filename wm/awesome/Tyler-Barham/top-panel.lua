5aa34915a3d5f3e3b5ddd4b53d7d278c6ecdd7f9
Tyler Barham <tyler.r.barham@gmail.com>
2025-01-07 09:12:32 +1300

    Use awesome-wm-widgets in top-panel

--- wm/awesome/Tyler-Barham/top-panel.lua
+++ wm/awesome/Tyler-Barham/top-panel.lua
@@ -4,15 +4,33 @@ local M = {}
 local gears = require("gears")
 local awful = require("awful")
 local wibox = require("wibox")
+local beautiful = require("beautiful")
+
 local keymaps = require("Tyler-Barham.keymaps")
 
+local volume_widget = require("awesome-wm-widgets.volume-widget.volume")
+local battery_widget = require("awesome-wm-widgets.batteryarc-widget.batteryarc")
+local calendar_widget = require("awesome-wm-widgets.calendar-widget.calendar")
+
+local panel_height = 32
+local widget_size = panel_height - 8
+local angle_length = 15
+local container_bg = "#111111"
+
+local systray = wibox.widget.systray()
+
 M.create = function(s)
+    systray:set_base_size(widget_size)
+    beautiful.bg_systray = container_bg
+    beautiful.systray_icon_spacing = 2
+
     local panel = awful.wibar({
         screen = s,
         position = "top",
-        height = 25,
+        height = panel_height,
         width = s.geometry.width,
-        opacity = 1
+        opacity = 1,
+        bg = gears.color.transparent
     })
 
     -- Create a wibox for each screen and add it
@@ -84,13 +102,11 @@ M.create = function(s)
 
     -- {{{ Wibar
     -- Create a textclock widget
-    local mytextclock = wibox.widget.textclock()
+    local mytextclock = wibox.widget.textclock("<span font='Grape Nuts Bold Italic 12'>%a %d %b - %T</span>", 1)
+    mytextclock.forced_width = 150
 
-    --calendar-widget
-    local mycalendar = awful.widget.calendar_popup.month({
-        position = "tc",
-        screen = s,
-        start_sunday = false,
+    local mycalendar = calendar_widget({
+        start_sunday = true,
     })
     mytextclock:connect_signal("button::press", function(_, _, _, button)
         if button == 1 then
@@ -99,6 +115,7 @@ M.create = function(s)
     end)
 
     local separator = wibox.widget.textbox("  ")
+    local separator_end = wibox.widget.textbox("        ")
 
     panel:setup {
         layout = wibox.layout.stack,
@@ -106,27 +123,68 @@ M.create = function(s)
         {
             layout = wibox.layout.align.horizontal,
             { -- Left widgets
-                layout = wibox.layout.fixed.horizontal,
-                separator,
-                mylauncher,
-                separator,
-                s.mytaglist,
-                separator,
-                s.mypromptbox,
+                widget = wibox.container.background,
+                bg = container_bg,
+                shape = function (cr, w, h)
+                    gears.shape.transform(gears.shape.rectangular_tag) : rotate_at(w/2, h/2, math.pi) (cr, w, h, angle_length)
+                end,
+                {
+                    layout = wibox.layout.fixed.horizontal,
+                    mylauncher,
+                    separator,
+                    s.mytaglist,
+                    separator_end,
+                }
             },
-            -- { layout = wibox.layout.fixed.horizontal, },
             nil,
             { -- Right widgets
-                layout = wibox.layout.fixed.horizontal,
-                wibox.widget.systray(),
-                separator,
+                widget = wibox.container.background,
+                bg = container_bg,
+                shape = function (cr, w, h)
+                    gears.shape.rectangular_tag(cr, w, h, angle_length)
+                end,
+                {
+                    layout = wibox.layout.fixed.horizontal,
+                    separator_end,
+                    volume_widget{
+                        widget_type = 'arc',
+                        size = widget_size,
+                    },
+                    separator,
+                    battery_widget({
+                        size = widget_size,
+                        font = 'Play ' .. panel_height/3,
+                        show_current_level = true,
+                        show_notification_mode = 'off',
+                    }),
+                    separator,
+                    {
+                        widget = wibox.container.place,
+                        valign = "center",
+                        systray
+                    },
+                    separator,
+                    separator,
+                }
             },
         },
         {
-            mytextclock,
             valign = "center",
             halign = "center",
             layout = wibox.container.place,
+            {
+                widget = wibox.container.background,
+                bg = container_bg,
+                shape = gears.shape.octogon,
+                {
+                    widget = wibox.container.margin,
+                    left = 25,
+                    right = 20,
+                    top = 4,
+                    bottom = 4,
+                    mytextclock
+                },
+            },
         },
     }
 end

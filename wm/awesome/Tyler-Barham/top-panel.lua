df4211df8a0e1921bfcf3ef8c143fc8395a3b0bf
Tyler Barham <tyler.r.barham@gmail.com>
2025-01-07 15:47:51 +1300

    Refac top-panel to powerline style

--- wm/awesome/Tyler-Barham/top-panel.lua
+++ wm/awesome/Tyler-Barham/top-panel.lua
@@ -11,10 +11,10 @@ local keymaps = require("Tyler-Barham.keymaps")
 local volume_widget = require("awesome-wm-widgets.volume-widget.volume")
 local battery_widget = require("awesome-wm-widgets.battery-widget.battery")
 local calendar_widget = require("awesome-wm-widgets.calendar-widget.calendar")
+local spotify_widget = require("awesome-wm-widgets.spotify-widget.spotify")
 
 local panel_height = 32
 local widget_size = panel_height - 8
-local angle_length = 15
 local container_bg = "#111111"
 
 local systray = wibox.widget.systray()
@@ -86,8 +86,78 @@ M.create = function(s)
     -- Create a taglist widget
     s.mytaglist = awful.widget.taglist {
         screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons
+        filter  = awful.widget.taglist.filter.noempty,
+        buttons = taglist_buttons,
+        style   = {
+            shape = gears.shape.powerline,
+            bg_occupied = container_bg,
+            bg_empty = container_bg,
+        },
+        layout  = {
+            spacing = -10,
+            spacing_widget = {
+                -- color = gears.color.transparent,
+                opacity = 0,
+                shape = gears.shape.powerline,
+                widget = wibox.widget.separator,
+            },
+            layout = wibox.layout.fixed.horizontal
+        },
+        widget_template = {
+            {
+                {
+                    {
+                        {
+                            {
+                                id     = 'index_role',
+                                widget = wibox.widget.textbox,
+                            },
+                            margins = 0,
+                            widget  = wibox.container.margin,
+                        },
+                        bg     = '#dddddd',
+                        shape  = gears.shape.circle,
+                        widget = wibox.container.background,
+                        visible = false
+                    },
+                    {
+                        {
+                            id     = 'icon_role',
+                            widget = wibox.widget.imagebox,
+                        },
+                        margins = 2,
+                        widget  = wibox.container.margin,
+                    },
+                    {
+                        id     = 'text_role',
+                        widget = wibox.widget.textbox,
+                    },
+                    layout = wibox.layout.fixed.horizontal,
+                },
+                left  = 18,
+                right = 18,
+                widget = wibox.container.margin
+            },
+            id     = 'background_role',
+            widget = wibox.container.background,
+            -- Add support for hover colors and an index label
+            create_callback = function(self, c3, index, objects) --luacheck: no unused args
+                self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
+                self:connect_signal('mouse::enter', function()
+                    if self.bg ~= '#ff0000' then
+                        self.backup     = self.bg
+                        self.has_backup = true
+                    end
+                    self.bg = '#ff0000'
+                end)
+                self:connect_signal('mouse::leave', function()
+                    if self.has_backup then self.bg = self.backup end
+                end)
+            end,
+            update_callback = function(self, c3, index, objects) --luacheck: no unused args
+                self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
+            end,
+        },
     }
 
     -- Create a tasklist widget
@@ -120,46 +190,61 @@ M.create = function(s)
         {
             layout = wibox.layout.align.horizontal,
             { -- Left widgets
-                widget = wibox.container.background,
-                bg = container_bg,
-                shape = function (cr, w, h)
-                    gears.shape.transform(gears.shape.rectangular_tag) : rotate_at(w/2, h/2, math.pi) (cr, w, h, angle_length)
-                end,
+                layout = wibox.layout.fixed.horizontal,
+                spacing = -10,
                 {
-                    layout = wibox.layout.fixed.horizontal,
-                    mylauncher,
-                    separator,
-                    s.mytaglist,
-                    separator_end,
-                }
+                    widget = wibox.container.background,
+                    bg = container_bg,
+                    shape = function (cr, w, h)
+                        gears.shape.transform(gears.shape.rectangular_tag) : rotate_at(w/2, h/2, math.pi) (cr, w, h, h/2)
+                    end,
+                    wibox.widget.textbox("     ")
+                },
+                s.mytaglist,
+                separator_end,
             },
             nil,
             { -- Right widgets
-                widget = wibox.container.background,
-                bg = container_bg,
-                shape = function (cr, w, h)
-                    gears.shape.rectangular_tag(cr, w, h, angle_length)
-                end,
+                layout = wibox.layout.fixed.horizontal,
+                spacing = -10,
                 {
-                    layout = wibox.layout.fixed.horizontal,
-                    separator_end,
-                    volume_widget{
-                        widget_type = 'icon_and_text',
-                    },
-                    separator,
-                    battery_widget({
-                        path_to_icons = '/usr/share/icons/gnome/scalable/status/',
-                        show_current_level = true,
-                    }),
-                    separator,
+                    widget = wibox.container.background,
+                    bg = container_bg,
+                    shape = function (cr, w, h)
+                        gears.shape.transform(gears.shape.powerline) : rotate_at(w/2, h/2, math.pi) (cr, w, h, h/2)
+                    end,
                     {
-                        widget = wibox.container.place,
-                        valign = "center",
-                        systray
-                    },
-                    separator,
-                    s.mylayoutbox,
-                    separator,
+                        layout = wibox.layout.fixed.horizontal,
+                        separator_end,
+                        volume_widget{
+                            widget_type = 'icon_and_text',
+                        },
+                        separator,
+                        battery_widget({
+                            path_to_icons = '/usr/share/icons/gnome/scalable/status/',
+                            show_current_level = true,
+                        }),
+                        separator_end,
+                    }
+                },
+                {
+                    widget = wibox.container.background,
+                    bg = container_bg,
+                    shape = function (cr, w, h)
+                        gears.shape.rectangular_tag(cr, w, h, h/2)
+                    end,
+                    {
+                        layout = wibox.layout.fixed.horizontal,
+                        separator_end,
+                        {
+                            widget = wibox.container.place,
+                            valign = "center",
+                            systray
+                        },
+                        separator,
+                        s.mylayoutbox,
+                        separator,
+                    }
                 }
             },
         },

5bfc10204780d650ffdcc7342b84b947f6bb2ec1
Tyler Barham <tyler.r.barham@gmail.com>
2025-01-08 11:14:52 +1300

    Copy over default theme

--- wm/awesome/rc.lua
+++ wm/awesome/rc.lua
@@ -48,7 +48,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "Tyler-Barham/themes/default/theme.lua")
 beautiful.useless_gap = 4
 
 -- This is used later as the default terminal and editor to run.

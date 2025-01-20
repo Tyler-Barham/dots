9010ea8524222ca8182778bb4e768b4c21620ba9
Tyler Barham <tyler.r.barham@gmail.com>
2025-01-20 15:37:30 +1300

    nvim link hl's for diff

--- editor/nvim/lua/Tyler-Barham/plugins/gruvbox.lua
+++ editor/nvim/lua/Tyler-Barham/plugins/gruvbox.lua
@@ -8,5 +8,9 @@ vim.g.gruvbox_baby_highlights = {
   VertSplit = { fg = c.milk }
 }
 
+vim.api.nvim_set_hl(0, '@diff.plus',  { link = 'DiffAdd' })
+vim.api.nvim_set_hl(0, '@diff.minus', { link = 'DiffDelete' })
+vim.api.nvim_set_hl(0, '@diff.delta', { link = 'DiffChange' })
+
 vim.cmd('colorscheme gruvbox-baby')
 

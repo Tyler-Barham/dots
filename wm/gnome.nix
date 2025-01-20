a6f189c9bdb79d9fbe510a13d274200e0f174fa1
Tyler Barham <tyler.r.barham@gmail.com>
2025-01-20 13:13:10 +1300

    Update xrandr.sh location

--- wm/gnome.nix
+++ wm/gnome.nix
@@ -4,6 +4,6 @@
 }: {
   home.file = {
     ".config/autostart/xrandr.desktop".source = ./xrandr.desktop;
-    ".config/autostart/xrandr.sh".source = ./xrandr.sh;
+    ".local/bin/xrandr.sh".source = ./xrandr.sh;
   };
 }

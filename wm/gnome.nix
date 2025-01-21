49ff40309ffb6df62729dffa1324c5f1ef80f06d
Tyler Barham <tyler.r.barham@gmail.com>
2025-01-22 10:21:25 +1300

    Gnome: list used extensions for later reference

--- wm/gnome.nix
+++ wm/gnome.nix
@@ -6,4 +6,15 @@
     ".config/autostart/xrandr.desktop".source = ./xrandr.desktop;
     ".local/bin/xrandr.sh".source = ./xrandr.sh;
   };
+
+  # Extensions
+  # AppIndicator and KStatusNotifier Support
+  # Bluetooth Quick Conneck
+  # Just Perfection
+  # Media Controls
+  # No overview at startup
+  # Open Bar
+  # Pop Shell
+  # Space Bar
+  # Top Bar Organizer
 }

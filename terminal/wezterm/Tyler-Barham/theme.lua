384aeb37267c5e6bec9f667cad0ed941eafaa48e
Tyler Barham <tyler.r.barham@gmail.com>
2025-01-31 17:07:22 +1300

    Wezterm: Make bg transparent rather than gif

--- terminal/wezterm/Tyler-Barham/theme.lua
+++ terminal/wezterm/Tyler-Barham/theme.lua
@@ -3,16 +3,16 @@ local wezterm = require('wezterm')
 
 function M.set_colors(config)
   config.color_scheme = 'Gruvbox Dark (Gogh)'
-  -- config.colors = {
-  --   background = 'black',
-  -- }
-  -- config.window_background_opacity = 0.8
-  config.window_background_image = wezterm.config_dir .. '/Tyler-Barham/assets/background.gif'
-  config.window_background_image_hsb = {
-    brightness = 0.05,
-    hue = 1.0,
-    saturation = 1.0,
+  config.colors = {
+    background = 'black',
   }
+  config.window_background_opacity = 0.9
+  -- config.window_background_image = wezterm.config_dir .. '/Tyler-Barham/assets/background.gif'
+  -- config.window_background_image_hsb = {
+  --   brightness = 0.05,
+  --   hue = 1.0,
+  --   saturation = 1.0,
+  -- }
 end
 
 function M.set_font(config)

26d8a4c8020845c33ceead2d25d427fc68502ec5
Tyler Barham <tyler.r.barham@gmail.com>
2025-02-21 11:39:43 +1300

    Nvim: Diff against blamed line

--- editor/nvim/lua/Tyler-Barham/keymaps/editor.lua
+++ editor/nvim/lua/Tyler-Barham/keymaps/editor.lua
@@ -26,3 +26,27 @@ vim.keymap.set('v', 'K', ':m\'<-2<CR>gv=gv', opts)
 -- Hide notification
 vim.keymap.set('n', '<leader><leader>', function() require('notify').dismiss() end, opts)
 
+-- View diff of HEAD and current lines last commit
+local function diffthis_linelastchange()
+  local cache = require('gitsigns.cache').cache
+  local winid = vim.api.nvim_get_current_win()
+  local bufnr = vim.api.nvim_win_get_buf(winid)
+  local lnum = vim.api.nvim_win_get_cursor(winid)[1]
+  local bcache = cache[bufnr]
+
+  if not bcache or not bcache.git_obj.object_name then
+    vim.notify('Cannot diff this line due to cache.', vim.log.levels.ERROR)
+    return
+  end
+  local blame_info = bcache:get_blame(lnum)
+  if not blame_info then
+    vim.notify('Cannot diff this line due to blame.', vim.log.levels.ERROR)
+    return
+  end
+  local commit = blame_info.commit.sha
+  local gitsigns = require('gitsigns')
+  gitsigns.diffthis(commit)
+end
+
+vim.keymap.set('n', '<leader>hd', diffthis_linelastchange, opts)
+

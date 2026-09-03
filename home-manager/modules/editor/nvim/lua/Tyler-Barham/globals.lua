ffb7de41a7304a4d9d32de88978b96fdbc1058e3
Tyler Barham <tyler.r.barham@gmail.com>
2026-09-04 10:22:56 +1200

    Nvim: Simplify clipboard

--- home-manager/modules/editor/nvim/lua/Tyler-Barham/globals.lua
+++ home-manager/modules/editor/nvim/lua/Tyler-Barham/globals.lua
@@ -2,18 +2,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '/'
 
-vim.g.clipboard = {
-  name = 'xclip',
-  copy = {
-    ['+'] = 'xclip -selection clipboard',
-    ['*'] = 'xclip -selection primary',
-  },
-  paste = {
-    ['+'] = 'xclip -selection clipboard -o',
-    ['*'] = 'xclip -selection primary -o',
-  },
-  cache_enabled = 0,
-}
+vim.g.clipboard = 'xclip'
 
 -- Providers
 vim.g.loaded_node_provider = 0
